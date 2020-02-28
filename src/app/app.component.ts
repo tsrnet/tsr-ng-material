cba32ff92c7d24fe44ad10f2c3f52de8012f473f
Antonio Dorado <antoniodavidorado@gmail.com>
2020-02-28 19:10:47 +0100

    Init project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tsr-ng-mat-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'TsrNgMat';
+}
